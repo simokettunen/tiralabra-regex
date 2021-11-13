298832814f086bfbc507e9427133e28fe1d861a5
Simo Kettunen <simo.kettunen@live.fi>
2021-11-13 21:06:27 +0200

    Rename test function names to be more informative

--- src/tests/nfa_test.py
+++ src/tests/nfa_test.py
@@ -2,7 +2,7 @@ import unittest
 from nfa import *
 
 class TestNFA(unittest.TestCase):
-    def test_empty(self):
+    def test_nfa_corresponding_to_empty_string_is_created_correctly(self):
         nfa = empty()
         
         self.assertSetEqual(nfa.states, {1, 2})
@@ -10,7 +10,7 @@ class TestNFA(unittest.TestCase):
         self.assertEqual(nfa.start_state, 1)
         self.assertEqual(nfa.accept_state, 2)
         
-    def test_single(self):
+    def test_nfa_corresponding_to_single_symbol_is_created_correctly(self):
         nfa = single('a')
         
         self.assertSetEqual(nfa.states, {1, 2})
@@ -18,7 +18,7 @@ class TestNFA(unittest.TestCase):
         self.assertEqual(nfa.start_state, 1)
         self.assertEqual(nfa.accept_state, 2)
         
-    def test_union(self):
+    def test_two_nfas_are_handled_correctly_by_union_operator(self):
         nfa1 = single('a')
         nfa2 = single('b')
         nfa = union(nfa1, nfa2)
@@ -28,7 +28,7 @@ class TestNFA(unittest.TestCase):
         self.assertEqual(nfa.start_state, 5)
         self.assertEqual(nfa.accept_state, 6)
         
-    def test_kleene_star(self):
+    def test_one_nfa_is_handled_correctly_by_kleene_star_operator(self):
         nfa1 = single('a')
         nfa = kleene_star(nfa1)
         
@@ -37,7 +37,7 @@ class TestNFA(unittest.TestCase):
         self.assertEqual(nfa.start_state, 3)
         self.assertEqual(nfa.accept_state, 4)
         
-    def test_concatenation(self):
+    def test_two_nfas_are_handled_correctly_by_concatenation_operator(self):
         nfa1 = single('a')
         nfa2 = single('b')
         nfa = concatenation(nfa1, nfa2)
