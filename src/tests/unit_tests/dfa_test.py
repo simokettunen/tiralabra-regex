6ee70146313e91b357721738a3067ceb229c56dd
Simo Kettunen <simo.kettunen@live.fi>
2021-12-21 02:54:01 +0200

    Fix existing tests and add two new tests

--- src/tests/unit_tests/dfa_test.py
+++ src/tests/unit_tests/dfa_test.py
@@ -2,7 +2,17 @@ import unittest
 from entities.dfa import DFA
 
 class TestDFA(unittest.TestCase):
-    def test_dfa_recognizes_union_operator_correctly(self):
+    def test_dfa_consisting_of_single_transition_works(self):
+        dfa = DFA()
+        dfa.add_state()
+        dfa.add_state()
+        dfa.add_transition(1, 2, 'a')
+        dfa.start_state = 1
+        dfa.accept_state = [2]
+        
+        self.assertTrue(dfa.match('a'))
+
+    def test_dfa_having_branch_works(self):
         dfa = DFA()
         dfa.add_state()
         dfa.add_state()
@@ -10,12 +20,12 @@ class TestDFA(unittest.TestCase):
         dfa.add_transition(1, 2, 'a')
         dfa.add_transition(1, 3, 'b')
         dfa.start_state = 1
-        dfa.accept_state = [2, 3]
+        dfa.accept_state = [2]
         
         self.assertTrue(dfa.match('a'))
-        self.assertTrue(dfa.match('b'))
+        self.assertFalse(dfa.match('b'))
         
-    def test_dfa_recognizes_kleene_star_operator_correctly(self):
+    def test_dfa_having_loop_works(self):
         dfa = DFA()
         dfa.add_state()
         dfa.add_state()
@@ -23,12 +33,12 @@ class TestDFA(unittest.TestCase):
         dfa.add_transition(1, 2, 'a')
         dfa.add_transition(2, 1, 'a')
         dfa.start_state = 1
-        dfa.accept_state = [1, 2]
+        dfa.accept_state = [2]
         
         self.assertTrue(dfa.match('a'))
-        self.assertTrue(dfa.match('aa'))
+        self.assertFalse(dfa.match('aa'))
         
-    def test_dfa_recognizes_concatenation_operator_correctly(self):
+    def test_dfa_having_two_sequential_transitions_and_accept_state_in_the_end_works(self):
         dfa = DFA()
         dfa.add_state()
         dfa.add_state()
@@ -40,3 +50,16 @@ class TestDFA(unittest.TestCase):
         
         self.assertTrue(dfa.match('ab'))
         self.assertFalse(dfa.match('a'))
+        
+    def test_dfa_having_two_sequential_transitions_and_accept_state_in_the_middle_works(self):
+        dfa = DFA()
+        dfa.add_state()
+        dfa.add_state()
+        dfa.add_state()
+        dfa.add_transition(1, 2, 'a')
+        dfa.add_transition(2, 3, 'b')
+        dfa.start_state = 1
+        dfa.accept_state = [2]
+        
+        self.assertFalse(dfa.match('ab'))
+        self.assertTrue(dfa.match('a'))
